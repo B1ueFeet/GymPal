fce6c157c8fa072e2b6d5a29e446ec8865c0bfb7
jimmy090110 <fabricioortega2011@gmail.com>
2022-08-29 15:57:40 -0500

    inventario

--- GymPal/src/com/vistas/VInventario.java
+++ GymPal/src/com/vistas/VInventario.java
@@ -2,10 +2,17 @@ package com.vistas;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Table;
 
 public class VInventario {
 
 	protected Shell shell;
+	private Button btnMerch;
 
 	/**
 	 * Launch the application.
@@ -42,7 +49,19 @@ public class VInventario {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		
+		btnMerch = new Button(shell, SWT.NONE);
+		btnMerch.setBounds(71, 53, 90, 30);
+		btnMerch.setText("Merch");
+		
+		Button btnProteinas = new Button(shell, SWT.NONE);
+		btnProteinas.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnProteinas.setBounds(250, 53, 90, 30);
+		btnProteinas.setText("Suplementos ");
 
 	}
-
 }
