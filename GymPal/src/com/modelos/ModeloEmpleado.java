06dc6bb5c8a38005b8dc253ddd1f9cc1fb1a36a6
B1ueFeet <b1uefeet.dev@gmail.com>
2022-09-03 11:41:55 -0500

    Verificacion de Usuario con correo e

--- GymPal/src/com/modelos/ModeloEmpleado.java
+++ GymPal/src/com/modelos/ModeloEmpleado.java
@@ -6,6 +6,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.objetos.Cliente;
 import com.objetos.Conexion;
@@ -18,9 +29,10 @@ public class ModeloEmpleado {
 	private Connection connection;
 	private PreparedStatement sentencia;
 	private boolean estadoOp;
+	private String correo;
 
 	// OBTENER USUARIO NOMBRE Y CONTRASEÑA
-	public Empleado verificarUsuario(String usr) throws SQLException {
+	public Empleado verificarExistencia(String usr) throws SQLException {
 		String sql = null;
 		ResultSet resultSet = null;
 		Empleado e = new Empleado();
@@ -28,17 +40,22 @@ public class ModeloEmpleado {
 		connection = obtenerConexion();
 		// select username,password,cargo,nombre,apellido from empleados where activo = 1 and username = 'socastillo'
 		try {
-			sql = "select username,password,privilegio from usuarios where activo = 1 and username = ?";
+			sql = "select * from empleados where activo = 1 and (username = ? or cedula = ?)";
 			sentencia = connection.prepareStatement(sql);
 			sentencia.setString(1, usr);
+			sentencia.setString(2, usr);
 			resultSet = sentencia.executeQuery();
 
 			if (resultSet.next()) {
-				e.setUserName(usr);
-				e.setPassword(resultSet.getString(2));
-				e.setCargo(resultSet.getInt(3));
-				e.setNombre(resultSet.getString(4));
-				e.setApellido(resultSet.getString(5));
+				e.setNombre(resultSet.getString(2));
+				e.setApellido(resultSet.getString(3));
+				e.setCedula(resultSet.getString(4));
+				e.setCelular(resultSet.getString(5));
+				e.setDireccion(resultSet.getString(6));
+				e.setCorreo(resultSet.getString(7));
+				e.setCargo(resultSet.getInt(8));
+				e.setUserName(resultSet.getString(9));
+				e.setPassword(resultSet.getString(10));
 			} else {
 				return null;
 			}
@@ -54,9 +71,67 @@ public class ModeloEmpleado {
 		return e;
 	}
 
-	private Connection obtenerConexion() throws SQLException {
-		return Conexion.getConnection();
 
-	}
+	// VERIFICAR ENVIAR CORREO
+		public boolean correo(String cedula) throws SQLException {
+			Empleado e = verificarExistencia(cedula);
+			if(e==null) {
+				return false;
+			}else {
+
+			System.out.println(e.toString());
+			enviarCorreo(e);
+			
+			return true;
+			}
+		}
+		
+		// ENVIAR CORREO
+		public void enviarCorreo(Empleado e) {
+
+	        String from = "gympal.app@outlook.com"; // Dirección de correo electrónico del remitente
+	        String to = e.getCorreo(); // Correo electrónico del destinatario
+	        // Obtiene las propiedades del sistema, que se utilizan principalmente para establecer parámetros relacionados con el correo electrónico.
+	        Properties properties = System.getProperties();
+	        // Configure el servidor de transferencia de correo. Dado que esta es una operación de envío de correo, necesitamos configurar el protocolo smtp. De acuerdo con los requisitos del correo de sincronización oficial de Outlook, configure la dirección del protocolo, el número de puerto y el método de cifrado a su vez
+	        properties.setProperty("mail.smtp.host", "smtp.office365.com");
+	        properties.setProperty("mail.smtp.port", "587");
+	        properties.setProperty("mail.smtp.starttls.enable", "true");
+	        // Verificación del usuario y volver a la sesión, activar la verificación del usuario, configurar la contraseña de la cuenta para enviar correo electrónico.
+	        properties.setProperty("mail.smtp.auth", "true");
+	        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+	            @Override
+	            protected PasswordAuthentication getPasswordAuthentication() {
+	                return new PasswordAuthentication("gympal.app@outlook.com", "mfdsa.3000"); // Contraseña de la cuenta
+	            }
+	        });
+
+	        // Cree un objeto de mensaje MimeMessage, el encabezado del mensaje se configura con la dirección del buzón de envío y recepción, y el cuerpo del mensaje contiene el título y el contenido del mensaje. Tipo de destinatario: TO significa envío directo, CC significa CC y BCC significa CC secreto.
+	        try {
+	            MimeMessage message = new MimeMessage(session);
+	            message.addHeader("X-Mailer", "Microsoft Outlook Express 6.00.2900.2869");
+	            message.setFrom(new InternetAddress(from));
+	            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	            message.setSubject("Recuperacion de Usuario");
+	            String correoe = String.format("Hola %s %s\nEste es el correo de recuperacion de"
+	            		+ " cuenta\ntus credenciales son:\n\nNombre de Usuario:		%s\nContraseña:				%s\n\n"
+	            		+ "Vuelve a la aplicacion para ingresar\nGracias por utilizar GYMpal", e.getNombre(),e.getApellido(),e.getUserName(),e.getPassword() );
+	            message.setText(correoe);
+
+	            Transport.send(message);
+	            System.out.println("success");
+	        } catch (MessagingException me) {
+	        	System.out.println("failed");
+	            me.printStackTrace();
+	            
+	        }
+
+	    }
+
+		private Connection obtenerConexion() throws SQLException {
+			return Conexion.getConnection();
+
+		}
+
 
 }
--- GymPal/src/com/vistas/CPrincipal.java
+++ GymPal/src/com/vistas/CPrincipal.java
@@ -2,6 +2,8 @@ package com.vistas;
 
 import org.eclipse.swt.widgets.Display;
 
+import com.controladores.ControlEmpleados;
+import com.modelos.ModeloEmpleado;
 import com.objetos.Empleado;
 
 
@@ -12,6 +14,7 @@ public class CPrincipal {
 	public CPrincipal() {
 	    	Display display = new Display();
 	    	//principal = new VPrincipal(display);
+	    	/*
 	    	VCarga carga  = new VCarga(display);
 	        //Splash splashWindow = new Splash(display);
 	        while((Display.getCurrent().getShells().length != 0) && !Display.getCurrent().getShells()[0].isDisposed())
@@ -20,14 +23,25 @@ public class CPrincipal {
 	             {
 	                 display.sleep();
 	             }
-	        }    	
+	        }  */
+	        ModeloEmpleado modeloU = new ModeloEmpleado();
+	        VUsuarios vistaU = new  VUsuarios(display);
+	        ControlEmpleados controlU =new ControlEmpleados(vistaU,modeloU);
+	       vistaU.setControlador(controlU);
+	       vistaU.open();
+	       while(!Display.getCurrent().getShells()[0].isDisposed())
+	        {
+	             if(!display.readAndDispatch())
+	             {
+	                 display.sleep();
+	             }
+	        } 
 	    }
 	
 	public static void main(String[] args) 
 	  {
-		Empleado emp = new Empleado(1, "S", "A", 1717171717, "0984653120", "recreo", "socastillo@gmail.com", Empleado.Cargo.GERENTE);
-		System.out.println(emp.getCargo());
 	    new CPrincipal();
 	  }
 
 }
+
